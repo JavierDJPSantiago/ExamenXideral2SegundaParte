304da91494ca4e84fdc80e13d4ee39c6efc65943
Javier <javier.de.jesus.perez.santiago@gmail.com>
2022-08-12 13:43:44 -0500

    01:43 pm añadi pruebas

--- composite-archivos/src/com/examen2/test/Examen2Test.java
+++ composite-archivos/src/com/examen2/test/Examen2Test.java
@@ -3,15 +3,28 @@ package com.examen2.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 
 import com.examen2.composite.*;
 
 class Examen2Test {
 
+	@BeforeEach
+	void beforeEach(TestInfo prueba) {
+		System.out.println("Iniciando prueba para " + prueba.getDisplayName());
+	}
+	
+	@AfterEach
+	void afterEach (TestInfo info) {
+		System.out.println("Terminando prueba para " + info.getDisplayName());
+
+	}
+	
 	@Test
 	void testString() {
 		String nombre = "nombre";
@@ -44,5 +57,39 @@ class Examen2Test {
 
 	}
 	
+	@Test
+	void performanceTest() {
+		List<ComponentDocumento> paginas = new ArrayList<>();
+		assertTimeout (Duration.ofSeconds(5), () ->{
+			for(int i = 0; i< 5 ; i++) {
+				ComponentDocumento indice = new LeafHoja("Indice");
+				paginas.add(indice);
+				int j = i;
+				System.out.println(j);
+			}
+		});
+	}
+	
+	@Test
+	void repetirTest() {
+		LeafHoja hoja = new LeafHoja("Hola");
+		assertFalse("hola como estas".contains(hoja.getNombre()));
+	}
+	
+	@Test
+	void tamañoForTest() {
+		List<ComponentDocumento> paginas = new ArrayList<>();
+		int j = paginas.size();
+		boolean a = false;
+		ComponentDocumento indice = new LeafHoja("Indice");
+		for(int i = 0; i < j; i++ ) {
+		paginas.add(indice);
+		a = true;
+		}
+		
+		assertEquals(false, a);
+	}
+	
+
 	
 }
